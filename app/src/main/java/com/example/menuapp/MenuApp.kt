fb5f4d799a1782e739a8d467fc2645cb130c1581
formaciondaniel21 <formaciondaniel21@gmail.com>
2025-04-22 19:51:37 +0200

    Inicio

---
+++ app/src/main/java/com/example/menuapp/MenuApp.kt
@@ -0,0 +1,29 @@
+package com.example.menuapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_app)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val buttonCalculator = findViewById<Button>(R.id.buttonCaculator)
+        buttonCalculator.setOnClickListener {navigateToCalculator}
+    }
+
+    fun navigateToCalculator() {
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+}
